49976e53386235438fbebb9bf4471a5bbfc01c61
padfoot-dev <abish102001@gmail.com>
2023-07-22 03:23:17 +0530

    router changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+
 import './index.css'
 import UserContextProvider from './contexts/UserContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-      <BrowserRouter>
+
       
       <UserContextProvider>
         <App />
         </UserContextProvider>
-        </BrowserRouter>
+
 
 
   </React.StrictMode>,
